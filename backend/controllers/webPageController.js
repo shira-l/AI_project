bc334099a6cd48bffd6d2a4110d3f6119df21590
דבורה אסתר קופינסקי <Hand4c27@mbjcomp.org.il>
2024-11-26 21:50:29 +0200

    good web prompt

--- backend/controllers/webPageController.js
+++ backend/controllers/webPageController.js
@@ -4,6 +4,16 @@ export async function getWebPage(req, res) {
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+    const header = 'Header and a tagline written in italics underneath the company name';
+    const navigation = 'Navigation to all sections of the web page ';
+    const content = 'Content section';
+    const about = 'About section- A paragraph describing the business ';
+    const social = 'Social proof for the business';
+    const benefits = 'A bulleted list of reasons why you should use our service and benefits';
+    const contact = 'My contact information';
+    const powerful = 'Powerful calls to action';
+    const footer = 'Footer in the end of the page';
+
     const chat = model.startChat({
         history: [
             {
@@ -17,15 +27,36 @@ export async function getWebPage(req, res) {
                         "Include the following ,making sure everything is customized to the details I am provide:" +
 
                         // "A company name that you think goes well with the business" +
-                        `${designDetails.sections.map(section => section == 'Header' ? 'Header and a tagline written in italics underneath the company name' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Navigation' ? 'Navigation to all sections of the web page ' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Content' ? 'Content section' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'About' ? 'About section- A paragraph describing the business ' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Social proof' ? 'Social proof for the business' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Benefits' ? 'A bulleted list of reasons why you should use our service and benefits' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Contact information' ? 'Contact information' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Powerful calls to action' ? 'Powerful calls to action' : null)} ` +
-                        `${designDetails.sections.map(section => section == 'Footer' ? 'Footer in the end of the page' : null)} ` +
+                        `${designDetails.sections.map(section => {
+                            switch (section) {
+                                case 'Header':
+                                    return header;
+                                case 'Navigation':
+                                    return navigation;
+                                case 'Content':
+                                    return contact;
+                                case 'About':
+                                    return about;
+                                case 'Social proof':
+                                    return social;
+                                case 'Benefits':
+                                    return benefits;
+                                case 'Contact information':
+                                    return contact;
+                                case 'Powerful calls to action':
+                                    return powerful;
+                                case 'Footer':
+                                    return footer;
+                            }
+                        })} ` +
+                        // `${designDetails.sections.map(section => section == 'Navigation' ? 'Navigation to all sections of the web page ' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Content' ? 'Content section' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'About' ? 'About section- A paragraph describing the business ' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Social proof' ? 'Social proof for the business' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Benefits' ? 'A bulleted list of reasons why you should use our service and benefits' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Contact information' ? 'Contact information' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Powerful calls to action' ? 'Powerful calls to action' : null)} ` +
+                        // `${designDetails.sections.map(section => section == 'Footer' ? 'Footer in the end of the page' : null)} ` +
                         "I'll provide you the relevant information. Ok? "
                 }],
             },
@@ -36,7 +67,7 @@ export async function getWebPage(req, res) {
         ],
     });
     console.log(designDetails);
-    let result = await chat.sendMessage(`my business information: name: ${businessDetails.name} email: ${businessDetails.email} companyType: ${businessDetails.companyType} description: ${businessDetails.description} purpose: ${businessDetails.purpose} aboute: ${businessDetails.aboute}.`);
+    let result = await chat.sendMessage(`my business information: name: ${designDetails.name} email: ${designDetails.email} companyType: ${designDetails.companyType} description: ${designDetails.description} purpose: ${designDetails.purpose} about: ${designDetails.about}.`);
     console.log(result.response.text());
 
 }
