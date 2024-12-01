30303510a7cb326e580116caf1c079a30296a201
דבורה אסתר קופינסקי <Hand4c27@mbjcomp.org.il>
2024-12-01 02:31:22 +0200

    name of business

--- backend/controllers/pdfFileController.js
+++ backend/controllers/pdfFileController.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import PDFDocument from 'pdfkit';
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import contentDisposition from 'content-disposition';
 
 export async function createCharacterizationFile(req, res) {
     const businessDetails = req.body;
@@ -9,36 +10,55 @@ export async function createCharacterizationFile(req, res) {
         // חיבור לג'מיני ליצירת תוכן
         const genAI = new GoogleGenerativeAI(process.env.API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const chat = model.startChat({
-            history: [
-                {
-                    role: "user",
-                    parts: [{
-                        text: `I want to open a new business. 
-              I want to give you category, my business idea, the purpose of the business and a description, 
-              and I want you to return to me a Business characterization document.
-              Return me the document in this json format, 
-              The keys should be: 'title', 'subTitle1', 'sectionHeading1', 'text1', 'sectionHeading2','text2', 'subTitle2','sectionHeading3', 'text3'. ext.
-              And values should be their content.
-              Manage with the information I give you dont ask questions and dont send eny thing else exept for the json. Ok?`
+        const chat = model.startChat();
 
-                    }],
-                },
-                {
-                    role: "model",
-                    parts: [{ text: "Absolutely! No problem. Just send me information." }],
-                },
-            ],
-        });
+        let resName = await chat.sendMessage(`I want to open a new business. 
+                Give me a creative good name for my company. 
+                Give me only a name, nothing else
+                I want to give you category, my business idea, the purpose of the business and a description, 
+                and I want you to return to me a company name.
+                My business information: 
+                Name: ${businessDetails.name}, 
+                Email: ${businessDetails.email}, 
+                Company Type: ${businessDetails.companyType}, 
+                Description: ${businessDetails.description}, 
+                Purpose: ${businessDetails.purpose}, 
+                Information: ${businessDetails.information}.`);
+        let passDomain = true;
+        let com = '.com';
+        let domain, name, domainName;
+        let companyName;
+        do {
+            try {
+                name = resName.response.text();
+                domainName = name.replaceAll(' ', '');
+                companyName = domainName;
+                domain = domainName.concat(com);
+                const url = `https://rdap.verisign.com/com/v1/domain/${domain}`;
+                const response = await fetch(url);
+                if (response.status === 200)
+                    resName = await chat.sendMessage("Give me a different name for my company. Give me only a name, nothing else");
+                else
+                    passDomain = false;
+            } catch (error) {
+                if (error.response && error.response.status === 404)
+                    passDomain = false;
+                else {
+                    console.error(`Error checking domain:`, error.message);
+                    //resName = await chat.sendMessage("Give me a different name for my company. Give me only a name, nothing else");
+                }
+            }
+        } while (passDomain);
+
 
         // שליחת המידע לג'מיני
-        const resText = await chat.sendMessage(`My business information: 
-      Name: ${businessDetails.name}, 
-      Email: ${businessDetails.email}, 
-      Company Type: ${businessDetails.companyType}, 
-      Description: ${businessDetails.description}, 
-      Purpose: ${businessDetails.purpose}, 
-      About: ${businessDetails.about}.`);
+        const resText = await chat.sendMessage(`
+              Return to me a Business characterization document for my ${name} business, 
+              The domain suggestion should be http://${domain},
+              The document in this json format, 
+              The keys should be: 'title', 'subTitle1', 'sectionHeading1', 'text1', 'sectionHeading2','text2', 'subTitle2','sectionHeading3', 'text3'. ext.
+              And values should be their content.
+              Manage with the information I gave you dont ask questions and dont send eny thing else exept for the json.`);
 
 
         // קבלת טקסט ותוכן לוגו
@@ -47,25 +67,6 @@ export async function createCharacterizationFile(req, res) {
         const pdfObject = JSON.parse(characterizationText.substring(characterizationText.indexOf('{'), characterizationText.lastIndexOf('}') + 1))
         console.log(pdfObject);
 
-        const resName = await chat.sendMessage("Give me a name for my company. Give me only a name, nothing else");
-        console.log(resName.response.text());
-        let name = resName.response.text();
-        name=name.replaceAll(' ', '');
-        let com = '.com';
-        const domain = name.concat(com);
-        const url = `https://rdap.verisign.com/com/v1/domain/${domain}`;
-        try {
-            const response = await fetch(url);
-            if (response.status === 200)
-                console.log(`Domain "${domain}" is taken.`);
-            else
-                console.log(`Domain "${domain}" might be available.`);
-        } catch (error) {
-            if (error.response && error.response.status === 404)
-                console.log(`Domain "${domain}" is available.`);
-            else
-                console.error(`Error checking domain:`, error.message);
-        }
 
         // יצירת מסמך PDF
         const doc = new PDFDocument();
@@ -102,15 +103,20 @@ export async function createCharacterizationFile(req, res) {
 
 
         doc.end();
-
+        // res.setHeader("content-disposition", contentDisposition(`${companyName}.pdf`));
         // שליחת המסמך ללקוח
         writeStream.on('finish', () => {
-            res.download(pdfPath, 'SpecificationFile.pdf', (err) => {
+            writeStream.close();
+            console.log(companyName);
+
+            res.set('Content-Disposition', 'attachment; filename=example.pdf');
+            res.download(pdfPath, `${companyName}.pdf`, (err) => {
                 if (err) {
                     console.error('Error sending file:', err);
                     res.status(500).send('Error generating PDF');
                 } else {
                     fs.unlinkSync(pdfPath); // מחיקת הקובץ מהשרת לאחר ההורדה
+
                 }
             });
         });
